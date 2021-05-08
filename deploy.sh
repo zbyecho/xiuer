83d86fcb37247a9bd189c2d9fc80c8c215d21714
张宝岩 <18735713467@163.com>
2021-05-08 13:14:49 +0800

    🚑

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@
 ###
  # @Date: 2021-05-07 10:41:15
  # @LastEditors: zhangbaoyan
- # @LastEditTime: 2021-05-07 10:45:54
+ # @LastEditTime: 2021-05-08 13:13:49
  # @FilePath: /xiuer/deploy.sh
 ### 
 # 当发生错误时中止脚本
@@ -17,14 +17,14 @@ cd docs
 # 部署到自定义域域名
 echo 'https://zbyecho.github.io/xiuer/' 
 
-git init
-git add -A
-git commit -m 'deploy'
+# git init
+# git add -A
+# git commit -m 'deploy'
 
 # 部署到 https://<USERNAME>.github.io
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f https://github.com/zbyecho/xiuer.git master:gh-pages
+git push -f https://zbyecho.github.io/xiuer/ master:gh-pages
 
 cd -
